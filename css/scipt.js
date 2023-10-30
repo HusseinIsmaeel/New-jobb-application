7134629b03e64469fcebce748bc9271246545e26
HusseinIsmaeel <63331212+HusseinIsmaeel@users.noreply.github.com>
2023-10-30 20:28:27 +0100

    Added job application form files

--- css/scipt.js
+++ css/scipt.js
@@ -3,3 +3,13 @@ document.getElementById('job-application-form').addEventListener('submit', funct
     // Här kan du lägga till kod för att hantera formulärdata
     alert('Formulär skickat!');
 });
+
+
+<script>
+    document.getElementById('job-application-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        alert('Tack för din ansökan!');
+        // Reset the form
+        e.target.reset();
+    });
+</script>
