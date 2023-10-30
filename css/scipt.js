bf76937d08a8bb6c67b5d642ca7df6a9b7deadb4
HusseinIsmaeel <63331212+HusseinIsmaeel@users.noreply.github.com>
2023-10-30 16:01:34 +0100

    Initial commit

---
+++ css/scipt.js
@@ -0,0 +1,5 @@
+document.getElementById('job-application-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Här kan du lägga till kod för att hantera formulärdata
+    alert('Formulär skickat!');
+});
